629c8cfb3f4afdde7892a799588a86418f5ed28b
alexis kriparakos <alexis.hennaut@gmail.com>
2020-02-10 13:28:56 +0200

    adds roundResult function

--- src/component/calculator/Calculator.js
+++ src/component/calculator/Calculator.js
@@ -8,247 +8,244 @@ import { evaluateCalc, multiplyTwoNumbers } from "../../utilities/calc";
 import "./styles/Calculator.css";
 
 class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      display: "",
-      input: "",
-      firstOperand: null,
-      secondOperand: null,
-      operator: null,
-      rates: {
-        USD: null,
-        JPY: null,
-        CAD: null,
-        SEK: null,
-        GBP: null
-      }
-    };
-  }
-  isNumber = value => !isNaN(value);
-  handleOperand = operator => {
-    let currentValue = this.state.input;
-    this.setState({
-      display: currentValue,
-      firstOperand: currentValue,
-      input: "",
-      operator: operator
-    });
-  };
-  handleSign = () => {
-    let currentValue = this.state.input;
-    currentValue = parseFloat(currentValue) * -1;
-    this.setState({ input: currentValue, display: currentValue });
-  };
-  handleEqual = () => {
-    this.state.secondOperand = this.state.input;
-    this.setState({
-      input: evaluateCalc(
-        this.state.operator,
-        this.state.firstOperand,
-        this.state.secondOperand
-      ),
-      display: evaluateCalc(
-        this.state.operator,
-        this.state.firstOperand,
-        this.state.secondOperand
-      )
-    });
-  };
-  handleDecimal = () => {
-    let currentValue = this.state.input;
-    currentValue = currentValue.includes(".")
-      ? currentValue
-      : currentValue + ".";
-    this.setState({
-      input: currentValue,
-      display: currentValue
-    });
-  };
-  handleBackSpace = () => {
-    let currentValue = this.state.input;
-    currentValue = currentValue.slice(0, -1);
-    this.setState({
-      input: currentValue,
-      display: currentValue
-    });
-  };
-  handleReset = () => {
-    this.setState({
-      input: "",
-      display: "",
-      firstOperand: "",
-      secondOperand: "",
-      operator: ""
-    });
-  };
-  handleExchange = currency => {
-    if (this.state.display) {
-      let currentValue = this.state.input;
-      this.setState({
-        display: multiplyTwoNumbers(currentValue, currency)
-      });
-    }
-  };
-  handleCurrency = currency => {
-    let currentValue = this.state.input;
-    let currencyRate;
-    if (this.state.display) {
-      if (currency === "$") {
-        currencyRate = this.state.rates.USD;
-        this.setState({
-          display: multiplyTwoNumbers(currentValue, currencyRate).toString()
-        });
-      } else if (currency === "¥") {
-        currencyRate = this.state.rates.JPY;
-        this.setState({
-          display: multiplyTwoNumbers(currentValue, currencyRate).toString()
-        });
-      }
-    }
-  };
-  handleNumber = value => {
-    let newInput = this.state.input + value;
-    if (newInput.length < 15) {
-      this.setState({
-        input: newInput,
-        display: newInput
-      });
-    }
-  };
+	constructor(props) {
+		super(props);
+		this.state = {
+			display: "",
+			input: "",
+			firstOperand: null,
+			secondOperand: null,
+			operator: null,
+			rates: {
+				USD: null,
+				JPY: null,
+				CAD: null,
+				SEK: null,
+				GBP: null,
+			},
+		};
+	}
+	isNumber = value => !isNaN(value);
+	handleOperand = operator => {
+		let currentValue = this.state.input;
+		this.setState({
+			display: currentValue,
+			firstOperand: currentValue,
+			input: "",
+			operator: operator,
+		});
+	};
+	handleSign = () => {
+		let currentValue = this.state.input;
+		currentValue = parseFloat(currentValue) * -1;
+		this.setState({ input: currentValue, display: currentValue });
+	};
+	handleEqual = () => {
+		this.state.secondOperand = this.state.input;
+		let currentValue = evaluateCalc(
+			this.state.operator,
+			this.state.firstOperand,
+			this.state.secondOperand
+		);
+		this.setState({
+			input: currentValue,
+			display: currentValue,
+		});
+	};
+	handleDecimal = () => {
+		let currentValue = this.state.input;
+		currentValue = currentValue.includes(".")
+			? currentValue
+			: currentValue + ".";
+		this.setState({
+			input: currentValue,
+			display: currentValue,
+		});
+	};
+	handleBackSpace = () => {
+		let currentValue = this.state.input;
+		currentValue = currentValue.slice(0, -1);
+		this.setState({
+			input: currentValue,
+			display: currentValue,
+		});
+	};
+	handleReset = () => {
+		this.setState({
+			input: "",
+			display: "",
+			firstOperand: "",
+			secondOperand: "",
+			operator: "",
+		});
+	};
+	handleExchange = currency => {
+		if (this.state.display) {
+			let currentValue = this.state.input;
+			this.setState({
+				display: multiplyTwoNumbers(currentValue, currency),
+			});
+		}
+	};
+	handleCurrencyBtn = currency => {
+		let currentValue = this.state.input;
+		let currencyRate;
+		if (this.state.display) {
+			if (currency === "$") {
+				currencyRate = this.state.rates.USD;
+				this.setState({
+					display: multiplyTwoNumbers(currentValue, currencyRate).toString(),
+				});
+			} else if (currency === "¥") {
+				currencyRate = this.state.rates.JPY;
+				this.setState({
+					display: multiplyTwoNumbers(currentValue, currencyRate).toString(),
+				});
+			}
+		}
+	};
+	handleNumber = value => {
+		let newInput = this.state.input + value;
+		if (newInput.length < 15) {
+			this.setState({
+				input: newInput,
+				display: newInput,
+			});
+		}
+	};
 
-  componentDidMount() {
-    const URL = `http://data.fixer.io/api/latest?
+	componentDidMount() {
+		const URL = `http://data.fixer.io/api/latest?
     access_key=2bfbded8300778f4269e781a93ff923b&
     symbols=USD,JPY,CAD,SEK,GBP&format=1`;
-    fetch(URL)
-      .then(data => data.json())
-      .then(data => {
-        let {
-          rates: { USD, JPY, CAD, SEK, GBP }
-        } = data;
-        this.setState({
-          rates: {
-            USD: USD,
-            JPY: JPY,
-            CAD: CAD,
-            SEK: SEK,
-            GBP: GBP
-          }
-        });
-      });
-  }
+		fetch(URL)
+			.then(currencyRates => currencyRates.json())
+			.then(currencyRates => {
+				let {
+					rates: { USD, JPY, CAD, SEK, GBP },
+				} = currencyRates;
+				this.setState({
+					rates: {
+						USD: USD,
+						JPY: JPY,
+						CAD: CAD,
+						SEK: SEK,
+						GBP: GBP,
+					},
+				});
+			});
+	}
 
-  render() {
-    const { rates } = this.state;
-    const country = ["USD", "JPY", "CAD", "SEK", "GBP"];
-    const exchangeCurrency = Object.values(rates).map((currency, index) => (
-      <ExchangeRates
-        country={country[index]}
-        currency={currency}
-        key={index}
-        handleClick={this.handleExchange}
-      />
-    ));
-    return (
-      <div className="app__wrapper">
-        <div className="calculator">
-          <DisplayScreen display={this.state.display} />
-          <div className="calculator__buttons">
-            <div className="buttons__row">
-              <Button isNumber={this.isNumber} handleClick={this.handleReset}>
-                C
-              </Button>
-              <Button isNumber={this.isNumber} handleClick={this.handleOperand}>
-                /
-              </Button>
-              <Button isNumber={this.isNumber} handleClick={this.handleOperand}>
-                *
-              </Button>
-              <Button
-                isNumber={this.isNumber}
-                handleClick={this.handleBackSpace}
-              >
-                CE
-              </Button>
-            </div>
-            <div className="buttons__row">
-              <Button isNumber={this.isNumber} handleClick={this.handleNumber}>
-                7
-              </Button>
-              <Button isNumber={this.isNumber} handleClick={this.handleNumber}>
-                8
-              </Button>
-              <Button isNumber={this.isNumber} handleClick={this.handleNumber}>
-                9
-              </Button>
-              <Button isNumber={this.isNumber} handleClick={this.handleOperand}>
-                -
-              </Button>
-            </div>
-            <div className="buttons__row">
-              <Button isNumber={this.isNumber} handleClick={this.handleNumber}>
-                4
-              </Button>
-              <Button isNumber={this.isNumber} handleClick={this.handleNumber}>
-                5
-              </Button>
-              <Button isNumber={this.isNumber} handleClick={this.handleNumber}>
-                6
-              </Button>
-              <Button isNumber={this.isNumber} handleClick={this.handleOperand}>
-                +
-              </Button>
-            </div>
-            <div className="buttons__row">
-              <Button isNumber={this.isNumber} handleClick={this.handleNumber}>
-                1
-              </Button>
-              <Button isNumber={this.isNumber} handleClick={this.handleNumber}>
-                2
-              </Button>
-              <Button isNumber={this.isNumber} handleClick={this.handleNumber}>
-                3
-              </Button>
-              <Button isNumber={this.isNumber} handleClick={this.handleEqual}>
-                =
-              </Button>
-            </div>
-            <div className="buttons__row">
-              <Button isNumber={this.isNumber} handleClick={this.handleSign}>
-                +/-
-              </Button>
-              <Button isNumber={this.isNumber} handleClick={this.handleNumber}>
-                0
-              </Button>
-              <Button isNumber={this.isNumber} handleClick={this.handleDecimal}>
-                .
-              </Button>
-              <Button isNumber={this.isNumber} handleClick={this.handleOperand}>
-                ^
-              </Button>
-            </div>
-            <div className="buttons__row">
-              <Button
-                isNumber={this.isNumber}
-                handleClick={this.handleCurrency}
-              >
-                $
-              </Button>
-              <Logo />
-              <Button
-                isNumber={this.isNumber}
-                handleClick={this.handleCurrency}
-              >
-                ¥
-              </Button>
-            </div>
-          </div>
-        </div>
-        <ul className="currency__list">{exchangeCurrency}</ul>
-      </div>
-    );
-  }
+	render() {
+		const { rates } = this.state;
+		const country = ["USD", "JPY", "CAD", "SEK", "GBP"];
+		const exchangeCurrency = Object.values(rates).map((currency, index) => (
+			<ExchangeRates
+				country={country[index]}
+				currency={currency}
+				key={index}
+				handleClick={this.handleExchange}
+			/>
+		));
+		return (
+			<div className="app__wrapper">
+				<div className="calculator">
+					<DisplayScreen display={this.state.display} />
+					<div className="calculator__buttons">
+						<div className="buttons__row">
+							<Button isNumber={this.isNumber} handleClick={this.handleReset}>
+								C
+							</Button>
+							<Button isNumber={this.isNumber} handleClick={this.handleOperand}>
+								/
+							</Button>
+							<Button isNumber={this.isNumber} handleClick={this.handleOperand}>
+								*
+							</Button>
+							<Button
+								isNumber={this.isNumber}
+								handleClick={this.handleBackSpace}
+							>
+								CE
+							</Button>
+						</div>
+						<div className="buttons__row">
+							<Button isNumber={this.isNumber} handleClick={this.handleNumber}>
+								7
+							</Button>
+							<Button isNumber={this.isNumber} handleClick={this.handleNumber}>
+								8
+							</Button>
+							<Button isNumber={this.isNumber} handleClick={this.handleNumber}>
+								9
+							</Button>
+							<Button isNumber={this.isNumber} handleClick={this.handleOperand}>
+								-
+							</Button>
+						</div>
+						<div className="buttons__row">
+							<Button isNumber={this.isNumber} handleClick={this.handleNumber}>
+								4
+							</Button>
+							<Button isNumber={this.isNumber} handleClick={this.handleNumber}>
+								5
+							</Button>
+							<Button isNumber={this.isNumber} handleClick={this.handleNumber}>
+								6
+							</Button>
+							<Button isNumber={this.isNumber} handleClick={this.handleOperand}>
+								+
+							</Button>
+						</div>
+						<div className="buttons__row">
+							<Button isNumber={this.isNumber} handleClick={this.handleNumber}>
+								1
+							</Button>
+							<Button isNumber={this.isNumber} handleClick={this.handleNumber}>
+								2
+							</Button>
+							<Button isNumber={this.isNumber} handleClick={this.handleNumber}>
+								3
+							</Button>
+							<Button isNumber={this.isNumber} handleClick={this.handleEqual}>
+								=
+							</Button>
+						</div>
+						<div className="buttons__row">
+							<Button isNumber={this.isNumber} handleClick={this.handleSign}>
+								+/-
+							</Button>
+							<Button isNumber={this.isNumber} handleClick={this.handleNumber}>
+								0
+							</Button>
+							<Button isNumber={this.isNumber} handleClick={this.handleDecimal}>
+								.
+							</Button>
+							<Button isNumber={this.isNumber} handleClick={this.handleOperand}>
+								^
+							</Button>
+						</div>
+						<div className="buttons__row">
+							<Button
+								isNumber={this.isNumber}
+								handleClick={this.handleCurrencyBtn}
+							>
+								$
+							</Button>
+							<Logo />
+							<Button
+								isNumber={this.isNumber}
+								handleClick={this.handleCurrencyBtn}
+							>
+								¥
+							</Button>
+						</div>
+					</div>
+				</div>
+				<ul className="currency__list">{exchangeCurrency}</ul>
+			</div>
+		);
+	}
 }
 
 export default Calculator;
--- src/utilities/calc.js
+++ src/utilities/calc.js
@@ -1,47 +1,48 @@
 const calculatorOperations = {
-  additionTwoNumbers: (firstNumber, secondNumber) => {
-    let result = parseFloat(firstNumber) + parseFloat(secondNumber);
-    return (result = Math.round(result * 100) / 100);
-  },
-  substractTwoNumbers: (firstNumber, secondNumber) => {
-    let result = parseFloat(firstNumber) - parseFloat(secondNumber);
-    return (result = Math.round(result * 100) / 100);
-  },
-  multiplyTwoNumbers: (firstNumber, secondNumber) => {
-    let result = parseFloat(firstNumber) * parseFloat(secondNumber);
-    return (result = Math.round(result * 100) / 100);
-  },
-  divideTwoNumbers: (firstNumber, secondNumber) => {
-    let result = parseFloat(firstNumber) / parseFloat(secondNumber);
-    return (result = Math.round(result * 100) / 100);
-  },
-  powerTwoNumbers: (firstNumber, secondNumber) => {
-    let result = parseFloat(firstNumber) ** parseFloat(secondNumber);
-    return (result = Math.round(result * 100) / 100);
-  }
+	additionTwoNumbers: (firstNumber, secondNumber) => {
+		let result = parseFloat(firstNumber) + parseFloat(secondNumber);
+		return calculatorOperations.roundResult(result);
+	},
+	substractTwoNumbers: (firstNumber, secondNumber) => {
+		let result = parseFloat(firstNumber) - parseFloat(secondNumber);
+		return roundResult(result);
+	},
+	multiplyTwoNumbers: (firstNumber, secondNumber) => {
+		let result = parseFloat(firstNumber) * parseFloat(secondNumber);
+		return roundResult(result);
+	},
+	divideTwoNumbers: (firstNumber, secondNumber) => {
+		let result = parseFloat(firstNumber) / parseFloat(secondNumber);
+		return roundResult(result);
+	},
+	powerTwoNumbers: (firstNumber, secondNumber) => {
+		let result = parseFloat(firstNumber) ** parseFloat(secondNumber);
+		return roundResult(result);
+	},
 };
+const roundResult = number => Math.round(number * 100) / 100;
 
 const evaluateCalc = (operation, number1, number2) => {
-  const {
-    additionTwoNumbers,
-    substractTwoNumbers,
-    multiplyTwoNumbers,
-    divideTwoNumbers,
-    powerTwoNumbers
-  } = calculatorOperations;
-  // eslint-disable-next-line
-  switch (true) {
-    case operation === "+":
-      return additionTwoNumbers(number1, number2).toString();
-    case operation === "-":
-      return substractTwoNumbers(number1, number2).toString();
-    case operation === "*":
-      return multiplyTwoNumbers(number1, number2).toString();
-    case operation === "/":
-      return divideTwoNumbers(number1, number2).toString();
-    case operation === "^":
-      return powerTwoNumbers(number1, number2).toString();
-  }
+	const {
+		additionTwoNumbers,
+		substractTwoNumbers,
+		multiplyTwoNumbers,
+		divideTwoNumbers,
+		powerTwoNumbers,
+	} = calculatorOperations;
+	// eslint-disable-next-line
+	switch (true) {
+		case operation === "+":
+			return additionTwoNumbers(number1, number2).toString();
+		case operation === "-":
+			return substractTwoNumbers(number1, number2).toString();
+		case operation === "*":
+			return multiplyTwoNumbers(number1, number2).toString();
+		case operation === "/":
+			return divideTwoNumbers(number1, number2).toString();
+		case operation === "^":
+			return powerTwoNumbers(number1, number2).toString();
+	}
 };
 const { multiplyTwoNumbers } = calculatorOperations;
 
